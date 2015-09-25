46c5fbfdb364194b4118eb078120555103de5e68
Ted Toal <twtoal@ucdavis.edu>
2015-09-25 11:13:33 -0700

    RUN doc improvements

---
+++ code/shell/makeDocs.sh
@@ -0,0 +1,6 @@
+source code/shell/makeHTML.sh README
+source code/shell/makeHTML.sh INSTALL
+source code/shell/makeHTML.sh RUN
+source code/shell/makePDF.sh README
+source code/shell/makePDF.sh INSTALL
+source code/shell/makePDF.sh RUN
