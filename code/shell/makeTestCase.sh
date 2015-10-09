442d052e4965c73e6985ea7f0b6f438e9530fe64
Ted Toal <twtoal@ucdavis.edu>
2015-10-08 17:27:40 -0700

    Improvements to makeTestCase.sh

--- code/shell/makeTestCase.sh
+++ code/shell/makeTestCase.sh
@@ -1,25 +1,45 @@
-# $1: target for CLEANING
+# $1: target for make
+# $2: target for make CLEAN=1  (default is same as $1, use 'NONE' to not clean)
 # Example: source code/shell/makeTestCase.sh ALL
 
-if [[ -z $1 ]] ; then
-    echo "Missing argument"
-    echo "Usage: source makeTestCase.sh <CLEAN target>"
+MAKETGT=$1
+CLEANTGT=$2
+if [[ -z $CLEANTGT ]] ; then
+    CLEANTGT=$MAKETGT
+fi
+
+if [[ -z $MAKETGT ]] ; then
+    echo "Usage: source makeTestCase.sh <make target> [<make CLEAN target>]"
+    echo "Default for <make CLEAN target> is same as <make target>"
+    echo "Use 'NONE' for <make CLEAN target> to not clean anything"
     echo "Example: source code/shell/makeTestCase.sh ALL"
-    echo "Use <CLEAN target> = NONE to not clean anything"
+    echo "Example: source code/shell/makeTestCase.sh ALL NONE"
+    echo "Example: source code/shell/makeTestCase.sh findIndelGroups"
     return
 fi
 
-if [[ $1 != "NONE" ]] ; then
-    echo "make PARAMS=allParameters.test CLEAN=1 $1"
-    make PARAMS=allParameters.test CLEAN=1 $1
+if [[ $CLEANTGT != "NONE" ]] ; then
+    echo "make PARAMS=allParameters.test CLEAN=1 $CLEANTGT"
+    make PARAMS=allParameters.test CLEAN=1 $CLEANTGT
+fi
+
+if [[ $CLEANTGT == "ALL" ]] ; then
+    echo "make PARAMS=allParameters.test CLEAN=1 plotIndels"
     make PARAMS=allParameters.test CLEAN=1 plotIndels
-    rm -f $1/MarkersAnnotated_*
+    echo "rm -f $CLEANTGT/MarkersAnnotated_*"
+    rm -f $CLEANTGT/MarkersAnnotated_*
 fi
 
 set -x #echo on
-make PARAMS=allParameters.test ALL | tee logFiles/makeLog_Test_$1.txt
-make PARAMS=allParameters.test plotIndels | tee logFiles/makeLog_Test_Indels.txt
-Rscript code/R/annotateFile.R annotate.template
-Rscript code/R/annotateFile.R annotate/HP11_isInNearColumn.markers
-Rscript code/R/annotateFile.R annotate/HP11_to_gff3.markers
+
+make PARAMS=allParameters.test $MAKETGT | tee logFiles/makeLog_Test_$MAKETGT.txt
+
+if [[ $MAKETGT == "ALL" ]] ; then
+    make PARAMS=allParameters.test plotIndels | tee logFiles/makeLog_Test_Indels.txt
+    Rscript code/R/dotplot.R dotplot.template | tee logFiles/makeLog_Test_dotplot.txt
+    Rscript code/R/annotateFile.R annotate.template | tee logFiles/makeLog_Test_annotate1.txt
+    Rscript code/R/annotateFile.R annotate/HP11_isInNearColumn.markers | tee logFiles/makeLog_Test_annotate2.txt
+    Rscript code/R/annotateFile.R annotate/HP11_to_gff3.markers | tee logFiles/makeLog_Test_annotate3.txt
+fi
+
 set +x #echo off
