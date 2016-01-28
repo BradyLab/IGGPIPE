17cc97c7a0528fa307a4df144c8e6c21ea887f4a
Ted Toal <twtoal@ucdavis.edu>
2016-01-27 21:07:05 -0800

    minor

--- code/shell/makeAllOtherTestCases.sh
+++ code/shell/makeAllOtherTestCases.sh
@@ -11,7 +11,3 @@ make PARAMS=allParameters/allParameters.HPT plotIndels | tee logFiles/makeLog_HP
 make PARAMS=allParameters/allParameters.HPT2.3 CLEAN=1 $1
 make PARAMS=allParameters/allParameters.HPT2.3 ALL | tee logFiles/makeLog_HPT2.3_$1.txt
 make PARAMS=allParameters/allParameters.HPT2.3 plotIndels | tee logFiles/makeLog_HPT2.3_IndelsSNPs.txt
-
-make PARAMS=allParameters/allParameters.Ta3 CLEAN=1 $1
-make PARAMS=allParameters/allParameters.Ta3 ALL | tee logFiles/makeLog_Ta3_$1.txt
-make PARAMS=allParameters/allParameters.Ta3 plotIndels | tee logFiles/makeLog_Ta3_IndelsSNPs.txt
